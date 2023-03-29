56ae74173b9be9e327952e998a999718cb473cf7
Fabian Zaremba <fabian@youremail.eu>
2023-03-29 13:25:19 +0200

    Code clean-up

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -232,9 +232,6 @@ function setSubjectPrefix(requestedClassification, callback) {
 
 
 function validateBody(event) {
-    /*mailboxItem.body.getAsync("html", {
-        asyncContext: event
-    }, checkBodyOnlyOnSendCallBack);*/
     forceClassificationSubject(event);
 }
 
@@ -302,97 +299,6 @@ function subjectOnSendChange(subject, event) {
 
         });
 }
-
-
-// Demo functions adapted from Microsoft
-// MIT License, https://github.com/OfficeDev/Office-Add-in-samples
-
-
-// Invoke by Contoso Subject and CC Checker add-in before send is allowed.
-// <param name="event">MessageSend event is automatically passed by BlockOnSend code to the function specified in the manifest.</param>
-function validateSubjectAndCC(event) {
-    shouldChangeSubjectOnSend(event);
-}
-
-// Check if the subject should be changed. If it is already changed allow send. Otherwise change it.
-// <param name="event">MessageSend event passed from the calling function.</param>
-function shouldChangeSubjectOnSend(event) {
-    mailboxItem.subject.getAsync({
-            asyncContext: event
-        },
-        function(asyncResult) {
-            addCCOnSend(asyncResult.asyncContext);
-            //console.log(asyncResult.value);
-            // Match string.
-            var checkSubject = (new RegExp(/\[Checked\]/)).test(asyncResult.value)
-            // Add [Checked]: to subject line.
-            subject = '[Checked]: ' + asyncResult.value;
-
-            // Check if a string is blank, null or undefined.
-            // If yes, block send and display information bar to notify sender to add a subject.
-            if (asyncResult.value === null || (/^\s*$/).test(asyncResult.value)) {
-                mailboxItem.notificationMessages.addAsync('NoSend', {
-                    type: 'errorMessage',
-                    message: 'Please enter a subject for this email.'
-                });
-                asyncResult.asyncContext.completed({
-                    allowEvent: false
-                });
-            } else {
-                // If can't find a [Checked]: string match in subject, call subjectOnSendChange function.
-                if (!checkSubject) {
-                    subjectOnSendChange(subject, asyncResult.asyncContext);
-                    //console.log(checkSubject);
-                } else {
-                    // Allow send.
-                    asyncResult.asyncContext.completed({
-                        allowEvent: true
-                    });
-                }
-            }
-
-        }
-    )
-}
-
-// Add a CC to the email.  In this example, CC contoso@contoso.onmicrosoft.com
-// <param name="event">MessageSend event passed from calling function</param>
-function addCCOnSend(event) {
-    mailboxItem.cc.setAsync(['Contoso@contoso.onmicrosoft.com'], {
-        asyncContext: event
-    });
-}
-
-
-// Check if the body contains a specific set of blocked words. If it contains the blocked words, block email from being sent. Otherwise allows sending.
-// <param name="asyncResult">MessageSend event passed from the calling function.</param>
-function checkBodyOnlyOnSendCallBack(asyncResult) {
-    var listOfBlockedWords = new Array("blockedword", "blockedword1", "blockedword2");
-    var wordExpression = listOfBlockedWords.join('|');
-
-    // \b to perform a "whole words only" search using a regular expression in the form of \bword\b.
-    // i to perform case-insensitive search.
-    var regexCheck = new RegExp('\\b(' + wordExpression + ')\\b', 'i');
-    var checkBody = regexCheck.test(asyncResult.value);
-
-    if (checkBody) {
-        mailboxItem.notificationMessages.addAsync('NoSend', {
-            type: 'errorMessage',
-            message: 'Blocked words have been found in the body of this email. Please remove them.'
-        });
-        // Block send.
-        asyncResult.asyncContext.completed({
-            allowEvent: false
-        });
-    } else {
-
-        // Allow send.
-        asyncResult.asyncContext.completed({
-            allowEvent: true
-        });
-    }
-}
-
 
 // Following functions adapted from easyEws
 // GNU Public License v3, https://github.com/davecra/easyEWS
